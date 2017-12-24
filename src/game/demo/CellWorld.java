046a7d241ee8fe416ad03a96791872304c456cf1
MicZcj <724390781@qq.com>
2017-12-24 23:36:53 +0800

    CellWorld初始化细胞生存时间！

---
+++ src/game/demo/CellWorld.java
@@ -0,0 +1,75 @@
+package game.demo;
+
+import java.awt.*;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.util.Random;
+
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class CellWorld extends JFrame {
+	int size;
+	int cellNum;
+	// 细胞存活图
+	int cells[][];
+	// 当前细胞存活图，用于记录本次细胞死亡存活状态，临时存放用
+	int currentCells[][];
+	// 界面
+	Container c = getContentPane();
+	JComponent[][] jps;
+
+	public CellWorld(int size, int cellNum) {
+		super();
+		// 多造外围的一圈就可以不用考虑边界了，也就是size+2
+		this.size = size + 2;
+		this.cellNum = cellNum;
+		cells = new int[this.size][this.size];
+		currentCells = new int[this.size][this.size];
+		initCells();
+		// 界面
+		c.setLayout(new GridLayout(size, size));
+		jps = new JComponent[this.size][this.size];
+		for (int i = 1; i < this.size - 1; i++) {
+			for (int j = 1; j < this.size - 1; j++) {
+				JPanel jp = new JPanel();
+				jps[i][j] = jp;
+				jp.setSize(20, 20);
+				jp.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+				jp.setBackground(Color.white);
+				getContentPane();
+				c.add(jp);
+			}
+		}
+
+		this.setSize(size * 20, size * 20);
+		this.setTitle("生命游戏");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
+
+	
+
+
+	// 根据输入的存活细胞数随机重置二维数组 用Random方法；
+	private void initCells() {
+		int i = 1;
+		while (this.cellNum > 0) {
+			for (int j = 1; j < size - 1; j++) {
+				if (cells[i][j] == 0 && Math.random() > 0.95) {
+					cells[i][j] = 1;
+					cellNum -= 1;
+				}
+			}
+			if (i == size - 2)
+				i = 1;
+			else
+				i += 1;
+		}
+		
+	}
+
+}
